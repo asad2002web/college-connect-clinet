4197782ddc6068e2c099e2023342a1214f7996a1
asad2002web <asad2002web@gmail.com>
2023-07-21 21:53:28 -0700

    initial setup and install

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <div className='text-2xl shadow-xl text-indigo-600 text-center flex items-center justify-center h-[100vh]'>Welcome to College Connect Website - Comming Soon</div>
+   </>
+  )
+}
+
+export default App
